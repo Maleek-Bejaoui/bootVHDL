3207478cd116d9663d468471fd964645fc54a046
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-02-24 17:00:41 +0100

    Update top.vhdl

--- src/top.vhdl
+++ src/top.vhdl
@@ -2,23 +2,23 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity tt_um_top is
+
+    entity tt_um_top is
     port (
-        ui_in1   : in  std_logic_vector(7 downto 0);
-        ui_in2   : in  std_logic_vector(7 downto 0);
         ui_in   : in  std_logic_vector(7 downto 0);
+        uo_out  : out std_logic_vector(7 downto 0);
+        uio_in  : in  std_logic_vector(7 downto 0);
+        uio_out : out std_logic_vector(7 downto 0);
+        uio_oe  : out std_logic_vector(7 downto 0);
+
         
-        uo_out1  : out std_logic_vector(7 downto 0);
-        uo_out2  : out std_logic_vector(7 downto 0);
-        uo_out3  : out std_logic_vector(7 downto 0);
-        uo_out4  : out std_logic_vector(7 downto 0);
-
-
         ena     : in  std_logic;
         clk     : in  std_logic;
         rst_n   : in  std_logic
     );
 end tt_um_top;
+
+    
 
 
     
@@ -42,28 +42,30 @@ architecture Behavioral of tt_um_top is
         ram_in : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
  end component ;
 
+        SIGNAL s_uio_out1,s_uio_out5 : STD_LOGIC_VECTOR(7 DOWNTO 0) ;
+        SIGNAL s_uio_out2,s_uio_out3, s_uio_out4,s_uio_out6 : STD_LOGIC_VECTOR(7 DOWNTO 0) ;
+
+
 begin
 
-    uo_out1(7 downto 4) <= "0000";
-    uo_out2(7 downto 6) <= "00";
+    uo_out(7 downto 1) <= "000000";
      U1 : boot_loader  
          generic map( RAM_ADR_WIDTH => 6,
             RAM_SIZE => 64)
          port map (rst => not(rst_n),
         clk => clk,
         ce => ena,
-        rx => ui_in1(0),
-        scan_memory => ui_in1(1),
-        ram_out(7 downto 0) => ui_in2, 
-        ram_out(15 downto 8) => ui_in,
                    
-        ram_rw => uo_out1(0),
-        ram_enable => uo_out1(1),
-        ram_adr => uo_out2(5 downto 0),
-        ram_in(7 downto 0) => uo_out3,
-        ram_in(15 downto 8) => uo_out4,
-        tx => uo_out1(2),
-        boot => uo_out1(3));
+        rx => ui_in(0),
+        scan_memory => ui_in(1),
+                   
+        ram_out(15 downto 0) => s_uio_out1,     
+        ram_rw => s_uio_out2,
+        ram_enable => s_uio_out3,
+        ram_adr => s_uio_out4,
+        ram_in(15 downto 0) => s_uio_out5,
+        tx => uo_out(0),
+        boot => s_uio_out6);
 
 
 end Behavioral;
